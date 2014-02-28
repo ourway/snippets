d505ba721026b186405c71e673ec8ebd3e5ad491
k1-hedayati <k1.hedayati93@gmail.com>
2014-02-28 13:33:12 +0330

    Updated and fixed checklink, switched to PyQuery

--- checklink.py
+++ checklink.py
@@ -4,6 +4,7 @@ import re
 import sys
 import os.path
 
+from pyquery import PyQuery as pq
 
 filename = sys.argv[1]
 path = sys.argv[2]
@@ -12,12 +13,11 @@ filelist = []
 with open(filename) as fd:
     for link in fd:
         try:
-            page = urlopen(link).read()
-            regex = re.compile('You have requested <font color="red">' +
-                               link[:-1] + '/(.*?)</font>', re.DOTALL)
-            filename = regex.search(str(page)).group(1)
-            filename.replace('*', '_')
+            page = pq(url=link, opener=lambda url, **kw: urlopen(url).read())
+            requested = page('div#content center font font').text()
+            filename = requested.split('/')[-1].replace('*', '_').strip()
             if not os.path.exists(os.path.join(path, filename)):
                 print(link[:-1] + ' ' + filename)
-        except (urllib.request.HTTPError, urllib.request.URLError):
-            print('Error in ' + link[:-1])
+        except (urllib.request.HTTPError, urllib.request.URLError,
+                AttributeError) as e:
+            print('Error in ' + link[:-1] + e.message)
